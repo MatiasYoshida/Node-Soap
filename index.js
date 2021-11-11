a80e47600f607b8b36752218af7b9cb081702366
Matias Yoshida <Matias.Yoshida@gmail.com>
2021-11-11 16:30:53 -0300

    Primera prueba v.0.0

---
+++ index.js
@@ -0,0 +1,45 @@
+const soap = require('soap');
+const express = require('express');
+
+const app = express();
+
+app.get('/Header_DOCLIT.xsd', (req, res) => {
+    res.sendFile(__dirname + '/wsdl/Header_DOCLIT.xsd');
+});
+
+app.get('/MultivasIsMigrated_v1.xsd', (req, res) => {
+    res.sendFile(__dirname + '/wsdl/MultivasIsMigrated_v1.xsd');
+});
+
+app.get('/MessageFault_DOCLIT.xsd', (req, res) => {
+    res.sendFile(__dirname + '/wsdl/MessageFault_DOCLIT.xsd');
+});
+
+
+
+var myService = {
+    MultivasIsMigrated: {
+        MultivasIsMigrated: {
+            isMigrated: (args) => {
+                const { snb } = args;
+                let respuesta = "OK"
+                return {
+                    respuesta: snb //Podemos tmb escribir respuesta solo y se expande automaticamente a respuesta:respuesta
+                };
+            },
+        }
+    }
+};
+
+
+var xml = require('fs').readFileSync('./wsdl/MultivasIsMigrated_v1.wsdl', 'utf8');
+
+app.listen(3030, () => {
+    console.log('listening on port 3030');
+    soap.listen(app, '/MultivasIsMigrated_v1', myService, xml, () => {
+        console.log('server initialized');
+    });
+});
+
+
+//http://127.0.0.1:3030/MultivasIsMigrated_v1?wsdl
